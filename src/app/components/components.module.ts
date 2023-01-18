8b899fbf8b75ed4e3e16e9cb0e9b344ed21dcb61
Jordisketch <hidalgojordi7@gmail.com>
2023-01-17 18:50:21 -0600

    cambios

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,6 +8,18 @@ import {CarouselComponent} from "./carousel/carousel.component";
 import { HomeComponent } from './home/home.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 import { ActivitesComponent } from './activites/activites.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatListModule} from "@angular/material/list";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [CarouselComponent, HomeComponent, TechnologiesComponent, ActivitesComponent],
@@ -17,6 +29,18 @@ import { ActivitesComponent } from './activites/activites.component';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatListModule,
+    FontAwesomeModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatGridListModule
   ],
 })
 export class ComponentsModule { }
